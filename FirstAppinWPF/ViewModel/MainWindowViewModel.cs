849cf053eacd666f8c05438f425090759c2703d6
Sheik Muthu <Sheik.Muthu@sisystems.com>
2020-10-16 15:24:07 +0530

    Add project files.

---
+++ FirstAppinWPF/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.MobileControls;
+using System.Windows.Input;
+
+namespace FirstAppinWPF.ViewModel
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public MainWindowViewModel()
+        {
+
+        }
+        private string _userName;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; OnPropertyChanged("UserName"); }
+        }
+
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; OnPropertyChanged("Password"); }
+        }
+        private ICommand _submitCommand;
+        public ICommand SubmitCommand
+        {
+            get
+            {
+                if (_submitCommand == null)
+                    _submitCommand = new Updater();
+                return _submitCommand;
+            }
+            set
+            {
+                _submitCommand = value;
+            }
+        }
+    }
+    class Updater : ICommand
+    {
+        #region ICommand Members  
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            //Your Code  
+        }
+        #endregion
+    }
+}
+
